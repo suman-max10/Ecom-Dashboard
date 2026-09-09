fd2d9d5a0f7d653d550fd06063699d1a64417f41
codersuman <sm093615@gmail.com>
2026-09-09 23:14:55 +0530

    Auth for login logout as well

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -4,4 +4,12 @@ const router = express.Router();
 router.get("/signup",(req,res)=>{
     res.send("Sign Up Routes Called");
 })
+
+router.get("/login",(req,res)=>{
+    res.send("Login Routes Called");
+})
+
+router.get("/logout",(req,res)=>{
+    res.send("Login Routes Called");
+})
 export default router;
