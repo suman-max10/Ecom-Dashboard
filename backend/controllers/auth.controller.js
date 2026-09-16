36c8d2fa140b931fe2583c36960240db38535f0f
codersuman <sm093615@gmail.com>
2026-09-16 23:45:56 +0530

    Update in auth controllers

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+/** @format */
+
+export const signup = (req, res) => {
+  res.send("Sign Up Routes Called");
+};
+
+export const login = (req, res) => {
+  res.send("Login Routes Called");
+};
+
+export const logout = (req, res) => {
+  res.send("Logout Routes Called");
+};
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,17 +1,12 @@
-import express from "express"
+/** @format */
+
+import express from "express";
+import { signup, login, logout } from "../controllers/auth.controller.js";
+
 const router = express.Router();
 
-router.get("/signup",(req,res)=>{
-    res.send("Sign Up Routes Called");
-})
+router.get("/signup", signup);
+router.get("/login", login);
+router.get("/logout", logout);
 
-router.get("/login",(req,res)=>{
-
-    res.send("Login Routes Called");
-})
-
-router.get("/logout",(req,res)=>{
-
-    res.send("Logout Routes Called");
-})
 export default router;
