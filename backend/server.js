b839151e4aa8e5e4f48eb87ffe27363060e241b9
codersuman <sm093615@gmail.com>
2026-09-16 23:46:58 +0530

    Update in Server.js

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,16 @@
+/** @format */
+
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 
-
 const app = express();
-dotenv.config()
-
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.use("/api/auth",authRoutes);
+app.use("/api/auth", authRoutes);
 
-
-
-app.listen(PORT,() => {
-
-    console.log("server is running on port", + PORT)
+app.listen(PORT, () => {
+  console.log("server is running on port", +PORT);
 });
-
