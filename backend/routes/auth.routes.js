a55884736ec023eaa138fe6eb3c6df4304a0d5fa
SUMAN MANDAL <128020279+suman-max10@users.noreply.github.com>
2026-09-17 23:58:09 +0530

    Update auth.routes.js

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -6,6 +6,8 @@ import { signup, login, logout } from "../controllers/auth.controller.js";
 const router = express.Router();
 
 router.get("/signup", signup);
+
+
 router.get("/login", login);
 router.get("/logout", logout);
 
